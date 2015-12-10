434d81c21d2984dbc2ce8a0812f43aa90e595c48
8BitRick <rickalo801@gmail.com>
2015-12-10 15:58:45 -0500

    FireFly Filter - Global Illumination Image Fixer
    This is intended to help detect the high intensity areas that appear
    incorrect. Then we will apply some discarding / merging solution to help
    that area look better.
    
    This is just checking the file with outline of steps to be taken in code.

---
+++ FireFlyFilter/FireFlyFilter.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# An attempt to find and filter out the outliers from monte carlo image
+# Assuming image created by Global Illumination monte carlo simulation
+
+# Load the file
+
+# Put into matricies
+
+# Save into new files
+
+# View each channel (R G B) independently
+
+# Convert to HSV
+# Save into separate H S V files
+
+# View those files - is the problem easy to see?
+
+# Make a map which clearly highlights the problem areas (the fire flies)
+
+# Use that map to fix our image
+# Fixing will probably be just blowing away our pixel and merging neighbor
+# information.
+
+# Save out the new image - DONE
